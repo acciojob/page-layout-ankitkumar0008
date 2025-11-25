66feb95698e6c5b6e6f04c3c11fcf255dae82bc9
ankit kumar <ankitkumar@ankits-MacBook-Pro.local>
2025-11-25 20:40:50 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,24 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <PageLayout header="Welcome to my website" footer="© 2023 My Website. All rights reserved">This is the content of my website.</PageLayout>
     </div>
   )
 }
 
 export default App
+
+function PageLayout({ header, footer, children }) {
+  return (
+    <>
+      
+      <header>{header}</header>
+
+      
+      <main>{children}</main>
+
+      
+      <footer>{footer}</footer>
+    </>
+  );
+}
